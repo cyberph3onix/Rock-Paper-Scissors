f727bbb5aa2b5d461e5b1c939933890f222604f9
Raaj Gupta <guptaraaj0505@gmail.com>
2024-12-21 00:21:27 +0530

    Update ropasc.py

--- ropasc.py
+++ ropasc.py
@@ -1,30 +1,45 @@
 import random
 
-while True:
-    # relating choices to emojis using dictionary
-    # taking tuple cuz we dont need to modify choices
-    choice = ('r','p','s')
-    emojis={'r':'🪨','p':'📃','s':'✂️'}
+# relating choices to emojis using dictionary
+# taking tuple cuz we dont need to modify choices
+choice = ('r','p','s')
+emojis={'r':'🪨','p':'📃','s':'✂️'}
 
-    #taking the choice of the user and letting computer choose 
-    user_choice = input('Rock, Paper or Scissor?!(r/p/s): ')
-    comp_choice = random.choice(choice)
-    if user_choice not in choice:
-        print ('invalid response')
-        continue
+#taking the choice of the user and letting computer choose 
+def get_user_choice ():
+    while True:    
+        user_choice = input('Rock, Paper or Scissor?!(r/p/s): ')
+        comp_choice = random.choice(choice)
+        if user_choice in choice:
+            return user_choice
+        else: 
+            print ('invalid response')
+    
 
-    #showing what the user chose and what the computer chose
+#showing what the user chose and what the computer chose
+def disp_user_choice(user_choice, comp_choice):
     print (f'User chose: {emojis[user_choice]}')
     print (f'Computer chose: {emojis[comp_choice]}')
 
-    #conditions for winning and loosing
+#conditions for winning and loosing
+def det_winner(user_choice, comp_choice):
     if (user_choice == 'r' and comp_choice == 's') or (user_choice == 's' and comp_choice == 'p') or (user_choice == 'p' and comp_choice == 'r'):
         print('you win')
     elif user_choice == comp_choice :
         print ('its a tie')
     else:
         print('you lost')
+
+#refactoring code and making it modular
+def play_game():
+    user_choice = get_user_choice()
+    comp_choice = random.choice(choice)
+    
+    disp_user_choice(user_choice,comp_choice)
+    det_winner(user_choice,comp_choice)
+    
     cont = input ('Wanna play more? (y/n): ')
     if cont == 'n':
         print ('thanks for playing!')
-        break
+
+play_game()
